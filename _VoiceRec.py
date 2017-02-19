cf6f5bf552ccaedce1cef56a196b485796fdb846
framework-Eleos <frameworkeleos@gmail.com>
2017-02-19 00:50:34 +0000

    Updated timeout length

--- _VoiceRec.py
+++ _VoiceRec.py
@@ -7,7 +7,7 @@ def Listen():
     with m as source:
         x = r.adjust_for_ambient_noise(source, duration = 1)
         print('[Listener]: Listening')
-        with stopit.ThreadingTimeout(5) as to_ctx_mgr:
+        with stopit.ThreadingTimeout(15) as to_ctx_mgr:
             assert to_ctx_mgr.state == to_ctx_mgr.EXECUTING
             audio = r.listen(source)
         if to_ctx_mgr.state == to_ctx_mgr.EXECUTED:
