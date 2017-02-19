3ab8956ef6fa4d273fdabeaba0ab7ddc0e38b55f
framework-Eleos <frameworkeleos@gmail.com>
2017-02-19 01:25:21 +0000

    Changed Main to use voice.py properly.

--- Main.py
+++ Main.py
@@ -1,13 +1,15 @@
-import _VoiceRec
+import _Voice
 import _Youtube
 
 
 #Assign:
 YoutubeSearch = _Youtube.YoutubeSearch
+Speak = _Voice.Speak
+
 
 while True:
-    Audio = _VoiceRec.Listen()
-    Recognized = _VoiceRec.Recognize(Audio)
+    Audio = _Voice.Listen()
+    Recognized = _Voice.Recognize(Audio)
     if Recognized != None:
         print('[I heard]: ' + str(Recognized))
     else:
