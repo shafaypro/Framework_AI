0a5f67ef9c3f0285900313fa924ce01cfbf3f628
framework-Eleos <frameworkeleos@gmail.com>
2017-02-19 00:46:40 +0000

    Mian script

---
+++ Main.py
@@ -0,0 +1,11 @@
+import _VoiceRec
+
+
+
+while True:
+    Audio = _VoiceRec.Listen()
+    Recognized = _VoiceRec.Recognize(Audio)
+    if Recognized != None:
+        print('[I heard]: ' + str(Recognized))
+    else:
+        pass
